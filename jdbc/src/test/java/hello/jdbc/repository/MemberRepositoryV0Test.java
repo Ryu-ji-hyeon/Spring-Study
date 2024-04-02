277dea1e7e92d65eee9ed3487a560d32448abaf2
Ji-Hyeon <dudvndwlgus@naver.com>
2024-04-02 15:22:38 +0900

    24.04.01 ADD : DB 조회

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -4,16 +4,20 @@ import hello.jdbc.domain.Member;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
-
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MemberRepository0Test {
+class MemberRepositoryV0Test {
 
     MemberRepository0 repository = new MemberRepository0();
-
     @Test
     void crud() throws SQLException {
-        Member member = new Member("memberV0", 10000);
+        //save
+        Member member = new Member("memberV3", 10000);
         repository.save(member);
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+
+        assertThat(findMember).isEqualTo(member);
     }
 }
